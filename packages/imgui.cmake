2f873079901c3f7e39536f1c166f8e004f729712
PAVEL PEREKHOZHIKH <p.perehogih@gmail.com>
2019-06-01 18:33:36 +0300

    Wip

--- packages/imgui.cmake
+++ packages/imgui.cmake
@@ -5,6 +5,6 @@ set(imgui_repo "https://github.com/ocornut/imgui")
 
 if (NOT EXISTS ${scpm_work_dir}/imgui)
         scpm_download_github_archive("${imgui_repo}" "v${imgui_version}")
-        file(DOWNLOAD "${scpm_server}/packages/${package_name}.cmake" "${scpm_work_dir}/imgui-${imgui_version}/CMakeLists.txt")
+        file(DOWNLOAD "${scpm_server}/packages/${package_name}.cmake.in" "${scpm_work_dir}/imgui-${imgui_version}/CMakeLists.txt")
         scpm_build_cmake("${scpm_work_dir}/imgui-${imgui_version}")
 endif()

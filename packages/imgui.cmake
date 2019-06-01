f942dfaf58c763c59b70fd8b5d4cbe54e4a9a15f
PAVEL PEREKHOZHIKH <p.perehogih@gmail.com>
2019-06-01 18:30:26 +0300

    Develop commit

--- packages/imgui.cmake
+++ packages/imgui.cmake
@@ -1,19 +1,10 @@
-set(IMGUI_VERSION "1.69")
-set(IMGUI_REPO "https://github.com/ocornut/imgui")
+if (NOT imgui_version)
+        set(imgui_version "1.70")
+endif()
+set(imgui_repo "https://github.com/ocornut/imgui")
 
-if (NOT EXISTS ${SCPM_WORK_DIR}/imgui)
-        scpm_download_github_archive("${IMGUI_REPO}" "v${IMGUI_VERSION}")
-        # execute_process(
-        #         COMMAND ${CMAKE_COMMAND} -E make_directory "${SCPM_WORK_DIR}/imgui/build"
-        #         WORKING_DIRECTORY "${SCPM_WORK_DIR}/imgui"
-        # )
-        # file(DOWNLOAD "${scpm_server}/packages/${package_name}.cmake" "${SCPM_WORK_DIR}/imgui/CMakeLists.txt")
-        # execute_process(
-        #         COMMAND ${CMAKE_COMMAND} .. -G "${CMAKE_GENERATOR}" -DCMAKE_INSTALL_PREFIX=${SCPM_ROOT_DIR}
-        #         WORKING_DIRECTORY "${SCPM_WORK_DIR}/imgui/build"
-        # )
-        # execute_process(
-        #         COMMAND ${CMAKE_COMMAND} --build . --target install --config Release
-        #         WORKING_DIRECTORY "${SCPM_WORK_DIR}/imgui/build"
-        # )
+if (NOT EXISTS ${scpm_work_dir}/imgui)
+        scpm_download_github_archive("${imgui_repo}" "v${imgui_version}")
+        file(DOWNLOAD "${scpm_server}/packages/${package_name}.cmake" "${scpm_work_dir}/imgui-${imgui_version}/CMakeLists.txt")
+        scpm_build_cmake("${scpm_work_dir}/imgui-${imgui_version}")
 endif()
--- scpm.cmake
+++ scpm.cmake
@@ -6,18 +6,62 @@ if (NOT scpm_server)
 endif()
 
 function(scpm_download_github_archive url filename)
-    file(DOWNLOAD ${url}/archive/${filename}.zip ${SCPM_WORK_DIR}/${filename}.zip)
+    message("[SCPM] download archive ${url}/archive/${filename}.zip to ${scpm_work_dir}/${filename}.zip")
+    file(DOWNLOAD ${url}/archive/${filename}.zip ${scpm_work_dir}/${filename}.zip)
+    message("[SCPM] extract archive ${scpm_work_dir}/${filename}.zip")
     execute_process(
-            COMMAND ${CMAKE_COMMAND} -E tar xzf ${SCPM_WORK_DIR}/${filename}.zip
-            WORKING_DIRECTORY ${SCPM_WORK_DIR}
+            COMMAND ${CMAKE_COMMAND} -E tar xzf ${scpm_work_dir}/${filename}.zip
+            WORKING_DIRECTORY ${scpm_work_dir}
     )
+    message("[SCPM] remove archive ${scpm_work_dir}/${filename}.zip")
+    file(REMOVE ${scpm_work_dir}/${filename}.zip)
 endfunction(scpm_download_github_archive)
 
+function(scpm_build_cmake directory)
+    message("[SCPM] begin build ${directory}")
+    execute_process(
+        COMMAND ${CMAKE_COMMAND} -E make_directory "${directory}/build"
+        WORKING_DIRECTORY "${directory}"
+    )
+    message("[SCPM] begin build ${directory}")
+    if (NOT MSVC)
+        execute_process(
+                COMMAND ${CMAKE_COMMAND} .. -G "${CMAKE_GENERATOR}" -DCMAKE_INSTALL_PREFIX=${scpm_root_dir} -DCMAKE_BUILD_TYPE=Release
+                WORKING_DIRECTORY "${directory}/build"
+        )
+        message("[SCPM] begin install ${directory}")
+        execute_process(
+                COMMAND ${CMAKE_COMMAND} --build . --target install --config Release
+                WORKING_DIRECTORY "${directory}/build"
+        )
+    else()
+        execute_process(
+                COMMAND ${CMAKE_COMMAND} .. -G "${CMAKE_GENERATOR}" -DCMAKE_INSTALL_PREFIX=${scpm_root_dir} -DDEBUG_POSTFIX=d
+                WORKING_DIRECTORY "${directory}/build"
+        )
+        message("[SCPM] begin install ${directory} Debug")
+        execute_process(
+                COMMAND ${CMAKE_COMMAND} --build . --target install --config Debug
+                WORKING_DIRECTORY "${directory}/build"
+        )
+        message("[SCPM] begin install ${directory} Release")
+        execute_process(
+                COMMAND ${CMAKE_COMMAND} --build . --target install --config Release
+                WORKING_DIRECTORY "${directory}/build"
+        )
+    endif()
+endfunction(scpm_build_cmake)
+
+
 function(scpm_install package_name package_version)
-    set(SCPM_WORK_DIR ${CMAKE_CURRENT_BINARY_DIR}/scpm)
-    set(SCPM_ROOT_DIR ${CMAKE_CURRENT_BINARY_DIR}/root)
+    set(scpm_work_dir ${CMAKE_CURRENT_BINARY_DIR}/scpm)
+    set(scpm_root_dir ${CMAKE_CURRENT_BINARY_DIR}/root)
     if (NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/scpm/${package_name}${package_version}.cmake)
-        file(DOWNLOAD ${scpm_server}/packages/${package_name}.cmake ${SCPM_WORK_DIR}/${package_name}${package_version}.cmake)
-        include(${SCPM_WORK_DIR}/${package_name}${package_version}.cmake)
+        message("[SCPM] download file ${scpm_server}/packages/${package_name}.cmake")
+        file(DOWNLOAD ${scpm_server}/packages/${package_name}.cmake ${scpm_work_dir}/${package_name}${package_version}.cmake)
+        if (package_version)
+            set("${package_name}_version" ${package_version})
+        endif()
+        include(${scpm_work_dir}/${package_name}${package_version}.cmake)
     endif()
 endfunction(scpm_install)

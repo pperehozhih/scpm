6bd673504f7affe1d83b011c90c2000e7f356022
PAVEL PEREKHOZHIKH <p.perehogih@gmail.com>
2019-06-03 10:05:42 +0300

    Chnage sfml to shared lib build

--- packages/sfml.cmake
+++ packages/sfml.cmake
@@ -6,29 +6,29 @@ set(sfml_repo "https://github.com/SFML/SFML")
 if (NOT EXISTS ${scpm_work_dir}/SFML-${sfml_version}.installed)
     scpm_download_github_archive("${sfml_repo}" "${sfml_version}")
     set(sfml_build_flags
-        -DBUILD_SHARED_LIBS=NO
+#        -DBUILD_SHARED_LIBS=NO
         -DCMAKE_INSTALL_PREFIX=${scpm_root_dir}
         -DSFML_MISC_INSTALL_PREFIX=${scpm_root_dir}/share/SFML
-        -DSFML_DEPENDENCIES_INSTALL_PREFIX=${scpm_root_dir}/Library/Frameworks
+        -DSFML_DEPENDENCIES_INSTALL_PREFIX=${scpm_root_dir}/Frameworks
     )
     scpm_build_cmake("${scpm_work_dir}/SFML-${sfml_version}" "${sfml_build_flags}")
     file(WRITE ${scpm_work_dir}/SFML-${sfml_version}.installed "")
 endif()
 
 set(sfml_lib
+    sfml-audio
+    sfml-graphics
+    sfml-network
+    sfml-system
+    sfml-window
+    CACHE STRING ""
+)
+
+set(sfml_lib_debug
     sfml-audio-s
     sfml-graphics-s
     sfml-network-s
     sfml-system-s
     sfml-window-s
-    CACHE STRING ""
-)
-
-set(sfml_lib_debug
-    sfml-audio-s-d
-    sfml-graphics-s-d
-    sfml-network-s-d
-    sfml-system-s-d
-    sfml-window-s-d
     CACHE STRING ""
 )

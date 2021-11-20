5b9743e363596b9f31f74882e4ed7f1a9af418de
Pavel Perekhozhikh <p.perehogih@gmail.com>
2021-11-20 22:44:54 +0300

    Change freetype location for download

--- packages/freetype.cmake
+++ packages/freetype.cmake
@@ -1,14 +1,24 @@
 if (NOT scpm_freetype_version)
-    set(scpm_freetype_version "2.10.0" CACHE STRING "")
+    set(scpm_freetype_version "VER-2-10-4" CACHE STRING "")
 endif()
 scpm_install(png)
 scpm_install(bzip2)
 scpm_install(zlib)
 scpm_install(harfbuzz)
-set(scpm_freetype_repo "https://download.savannah.gnu.org/releases/freetype")
+set(scpm_freetype_repo "https://github.com/freetype/freetype.git")
 
 if (NOT EXISTS ${scpm_work_dir}/freetype-${scpm_freetype_version}.installed)
-    scpm_download_and_extract_archive("${scpm_freetype_repo}" "freetype-${scpm_freetype_version}.tar.bz2")
+    if(EXISTS "${scpm_work_dir}/freetype-${scpm_freetype_version}")
+        file(REMOVE_RECURSE "${scpm_work_dir}/freetype-${scpm_freetype_version}")
+    endif()
+    execute_process(
+        COMMAND git clone -b ${scpm_freetype_version} --depth 1 ${scpm_freetype_repo} freetype-${scpm_freetype_version}
+        WORKING_DIRECTORY ${scpm_work_dir}
+        RESULT_VARIABLE scpm_freetype_clone_reult
+    )
+    if (NOT scpm_freetype_clone_reult EQUAL "0")
+        message(FATAL_ERROR "[SCPM] cannot clone repos ${scpm_freetype_repo}")
+    endif()
     scpm_build_cmake("${scpm_work_dir}/freetype-${scpm_freetype_version}" "")
     if (EXISTS "${scpm_root_dir}/lib/libfreetyped.a")
         file(RENAME "${scpm_root_dir}/lib/libfreetyped.a" "${scpm_root_dir}/lib/libfreetype.a")

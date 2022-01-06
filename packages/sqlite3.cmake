e184c0b6ea57dcb88f5d40b3a8cf2cd80d62edce
Pavel Perekhozhikh <p.perehogih@gmail.com>
2022-01-07 01:36:03 +0300

    Добавил sqlite3

---
+++ packages/sqlite3.cmake
@@ -0,0 +1,26 @@
+if (NOT scpm_sqlite3_version)
+	set(scpm_sqlite3_version "3370200" CACHE STRING "")
+endif()
+set(scpm_sqlite3_repo "https://www.sqlite.org/2022/")
+
+if (NOT EXISTS ${scpm_work_dir}/sqlite3-${scpm_sqlite3_version}.installed)
+	scpm_download_and_extract_archive("${scpm_sqlite3_repo}/${scpm_sqlite3_version}/" "sqlite-amalgamation-${scpm_sqlite3_version}.zip")
+	# https://www.sqlite.org/2022/sqlite-amalgamation-3370200.zip
+	scpm_build_cmake("${scpm_work_dir}/sqlite3-${scpm_sqlite3_version}")
+	file(WRITE ${scpm_work_dir}/sqlite3-${scpm_sqlite3_version}.installed "")
+endif()
+
+set(scpm_sqlite3_lib
+	sqlite3
+	CACHE STRING ""
+)
+
+set(scpm_sqlite3_lib_debug
+	sqlite3d
+	CACHE STRING ""
+)
+
+set(scpm_sqlite3_depends
+	""
+	CACHE STRING ""
+)

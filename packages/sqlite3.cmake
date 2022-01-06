2adb6eca2df4124875b325e58aa4accedc55d443
Pavel Perekhozhikh <p.perehogih@gmail.com>
2022-01-07 02:05:37 +0300

    Added CMake file for sqlite

--- packages/sqlite3.cmake
+++ packages/sqlite3.cmake
@@ -6,7 +6,8 @@ set(scpm_sqlite3_repo "https://www.sqlite.org/2022/")
 if (NOT EXISTS ${scpm_work_dir}/sqlite3-${scpm_sqlite3_version}.installed)
 	scpm_download_and_extract_archive("${scpm_sqlite3_repo}" "sqlite-amalgamation-${scpm_sqlite3_version}.zip")
 	# https://www.sqlite.org/2022/sqlite-amalgamation-3370200.zip
-	scpm_build_cmake("${scpm_work_dir}/sqlite-${scpm_sqlite3_version}")
+	file(DOWNLOAD "${scpm_server}/packages/sqlite3.cmake.in" "${scpm_work_dir}/sqlite-amalgamation-${scpm_sqlite3_version}/CMakeLists.txt")
+	scpm_build_cmake("${scpm_work_dir}/sqlite-amalgamation-${scpm_sqlite3_version}")
 	file(WRITE ${scpm_work_dir}/sqlite3-${scpm_sqlite3_version}.installed "")
 endif()
 

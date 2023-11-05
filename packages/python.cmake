97145ba30d7cda87e5ffc7b3e59566cbebee1c40
Pavel Perekhozhikh <p.perehogih@gmail.com>
2023-11-05 14:15:11 +0300

    Draft added python

---
+++ packages/python.cmake
@@ -0,0 +1,26 @@
+if (NOT scpm_python_version)
+    set(scpm_python_version "v3.10.11" CACHE STRING "")
+endif()
+set(scpm_python_repo "https://github.com/python/cpython")
+
+if (NOT EXISTS ${scpm_work_dir}/python-${scpm_python_version}.installed)
+    scpm_download_github_archive("${scpm_python_repo}" "${scpm_python_version}")
+    scpm_build_configure("${scpm_work_dir}/python-${scpm_python_version}")
+    scpm_build_make("${scpm_work_dir}/python-${scpm_python_version}")
+    file(WRITE ${scpm_work_dir}/python-${scpm_python_version}.installed "")
+endif()
+
+set(scpm_python_lib
+    ""
+    CACHE STRING ""
+)
+
+set(scpm_python_lib_debug
+    ""
+    CACHE STRING ""
+)
+
+set(scpm_python_depends
+    ""
+    CACHE STRING ""
+)

a0315e88403a56b6ba396edadf57d1b6f8040298
Pavel Perekhozhikh <p.perehogih@gmail.com>
2023-11-05 14:42:18 +0300

    WIP Python

--- packages/python.cmake
+++ packages/python.cmake
@@ -1,10 +1,10 @@
 if (NOT scpm_python_version)
-    set(scpm_python_version "v3.12.0" CACHE STRING "")
+    set(scpm_python_version "3.12.0" CACHE STRING "")
 endif()
 set(scpm_python_repo "https://github.com/python/cpython")
 
 if (NOT EXISTS ${scpm_work_dir}/python-${scpm_python_version}.installed)
-    scpm_download_github_archive("${scpm_python_repo}" "${scpm_python_version}")
+    scpm_download_github_archive("${scpm_python_repo}" "v${scpm_python_version}")
     scpm_build_configure("${scpm_work_dir}/python-${scpm_python_version}")
     scpm_build_make("${scpm_work_dir}/python-${scpm_python_version}")
     file(WRITE ${scpm_work_dir}/python-${scpm_python_version}.installed "")

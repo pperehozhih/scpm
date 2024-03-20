1284f39969f5f36fb52dfcfabd19605dbc09eae2
Pavel Perekhozhikh <p.perehogih@gmail.com>
2024-03-20 14:00:23 +0300

    Fix bug for build sqlite3_geo

--- packages/sqlite3_geo.cmake
+++ packages/sqlite3_geo.cmake
@@ -4,7 +4,7 @@ endif()
 set(scpm_sqlite3_geo_repo "https://gitflic.ru/project/paul2la/sqlite3-with-geo-distance.git")
 
 if (NOT EXISTS ${scpm_work_dir}/sqlite3-geo-${scpm_sqlite3_geo_version}.installed)
-	scpm_clone_git("${scpm_png_repo}" "v${scpm_png_version}")
+	scpm_clone_git("${scpm_png_repo}" "v${scpm_sqlite3_geo_version}")
 	scpm_build_cmake("${scpm_work_dir}/sqlite3-with-geo-distance")
 	file(WRITE ${scpm_work_dir}/sqlite3-geo-${scpm_sqlite3_geo_version}.installed)
 endif()

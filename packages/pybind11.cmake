9416adccbd69216e791c7e1e4864cc3aa1f5aa33
Pavel Perekhozhikh <p.perehogih@gmail.com>
2024-08-03 10:39:31 +0300

    disbale pybind test

--- packages/pybind11.cmake
+++ packages/pybind11.cmake
@@ -5,7 +5,7 @@ set(scpm_pybind_repo "https://github.com/pybind/pybind11")
 
 if (NOT EXISTS ${scpm_work_dir}/pybind-${scpm_pybind_version}.installed)
     scpm_download_github_archive("${scpm_pybind_repo}" "v${scpm_pybind_version}")
-    scpm_build_cmake("${scpm_work_dir}/pybind11-${scpm_pybind_version}" "-DPython_ROOT_DIR=\"${PYTHON_ROOT_DIR}\"")
+    scpm_build_cmake("${scpm_work_dir}/pybind11-${scpm_pybind_version}" "-DPython_ROOT_DIR=\"${PYTHON_ROOT_DIR}\" -DPYBIND11_TEST=OFF")
     file(WRITE ${scpm_work_dir}/pybind-${scpm_pybind_version}.installed)
 endif()
 

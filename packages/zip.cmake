0542503cb73893297264d503e7f4852b698b8831
Pavel Perekhozhikh <p.perehogih@gmail.com>
2024-10-02 20:15:15 +0300

    Disable zstd and lzma for libzip

--- packages/zip.cmake
+++ packages/zip.cmake
@@ -5,7 +5,7 @@ set(scpm_zip_repo "https://github.com/nih-at/libzip")
 
 if (NOT EXISTS ${scpm_work_dir}/zip-${scpm_zip_version}.installed)
     scpm_download_github_archive("${scpm_zip_repo}" "v${scpm_zip_version}")
-    scpm_build_cmake("${scpm_work_dir}/libzip-${scpm_zip_version}" "-DBUILD_SHARED_LIBS=OFF")
+    scpm_build_cmake("${scpm_work_dir}/libzip-${scpm_zip_version}" "-DBUILD_SHARED_LIBS=OFF -DENABLE_OPENSSL=OFF -DENABLE_MBEDTLS=OFF -DENABLE_GNUTLS=OFF -DENABLE_COMMONCRYPTO=OFF -DENABLE_WINDOWS_CRYPTO=OFF -DENABLE_LZMA=OFF -DENABLE_ZSTD=OFF")
     file(WRITE ${scpm_work_dir}/zip-${scpm_zip_version}.installed)
 endif()
 if(scpm_platform_windows)

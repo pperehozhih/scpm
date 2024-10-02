bb853a41a136f785f986c39bd9832db7a7c1ebdd
Pavel Perekhozhikh <p.perehogih@gmail.com>
2024-10-02 09:17:28 +0300

    Change libzip to static version

--- packages/zip.cmake
+++ packages/zip.cmake
@@ -5,7 +5,7 @@ set(scpm_zip_repo "https://github.com/nih-at/libzip")
 
 if (NOT EXISTS ${scpm_work_dir}/zip-${scpm_zip_version}.installed)
     scpm_download_github_archive("${scpm_zip_repo}" "v${scpm_zip_version}")
-    scpm_build_cmake("${scpm_work_dir}/libzip-${scpm_zip_version}")
+    scpm_build_cmake("${scpm_work_dir}/libzip-${scpm_zip_version}" "-DBUILD_SHARED_LIBS=OFF")
     file(WRITE ${scpm_work_dir}/zip-${scpm_zip_version}.installed)
 endif()
 if(scpm_platform_windows)

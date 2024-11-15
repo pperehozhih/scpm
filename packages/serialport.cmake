4a86052765f3ddf8c9a097fffb1826f1bb30982e
Pavel Perekhozhikh <p.perehogih@gmail.com>
2024-11-15 11:45:13 +0300

    Fix serial port build

--- packages/serialport.cmake
+++ packages/serialport.cmake
@@ -1,11 +1,11 @@
 if (NOT scpm_serialport_version)
     set(scpm_serialport_version "libserialport-0.1.2-cmake" CACHE STRING "")
 endif()
-set(scpm_bzip2_repo "https://gitflic.ru/project/paul2la/libserialport.git")
+set(scpm_serialport_repo "https://gitflic.ru/project/paul2la/libserialport.git")
 
 if (NOT EXISTS ${scpm_work_dir}/serialport-${scpm_serialport_version}.installed)
     scpm_clone_git("${scpm_serialport_repo}" "${scpm_serialport_version}")
-    scpm_build_cmake("${scpm_work_dir}/serialport")
+    scpm_build_cmake("${scpm_work_dir}/libserialport")
     file(WRITE ${scpm_work_dir}/serialport-${scpm_serialport_version}.installed)
 endif()
 

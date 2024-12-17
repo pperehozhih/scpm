c7730734b26c73738971522df328a8a0332f0c44
Pavel Perekhozhikh <p.perehogih@gmail.com>
2024-12-17 22:27:09 +0300

    Wolfssl fix build for android

--- packages/wolfssl.cmake
+++ packages/wolfssl.cmake
@@ -5,7 +5,7 @@ set(scpm_wolfssl_repo "https://github.com/wolfSSL/wolfssl")
 
 if (NOT EXISTS ${scpm_work_dir}/wolfssl-${scpm_wolfssl_version}.installed)
     scpm_download_github_archive("${scpm_wolfssl_repo}" "v${scpm_wolfssl_version}")
-    scpm_build_cmake("${scpm_work_dir}/wolfssl-${scpm_wolfssl_version}" "-DBUILD_SHARED_LIBS=OFF" "-DWOLFSSL_OPENSSLEXTRA=ON")
+    scpm_build_cmake("${scpm_work_dir}/wolfssl-${scpm_wolfssl_version}" "-DBUILD_SHARED_LIBS=OFF" "-DWOLFSSL_OPENSSLEXTRA=ON" "-DCRYPT_TESTS_DEFAULT=OFF")
     file(WRITE ${scpm_work_dir}/wolfssl-${scpm_wolfssl_version}.installed)
 endif()
 

37958d85e522a149ee88c097f83ccfc3a19ed3de
Pavel Perekhozhikh <p.perehogih@gmail.com>
2024-12-17 17:30:05 +0300

    Add wolf ssl

---
+++ packages/wolfssl.cmake
@@ -0,0 +1,36 @@
+if (NOT scpm_wolfssl_version)
+    set(scpm_wolfssl_version "5.7.4-stable" CACHE STRING "")
+endif()
+set(scpm_wolfssl_repo "https://github.com/wolfSSL/wolfssl")
+
+if (NOT EXISTS ${scpm_work_dir}/wolfssl-${scpm_wolfssl_version}.installed)
+    scpm_download_github_archive("${scpm_wolfssl_repo}" "v${scpm_wolfssl_version}")
+    scpm_build_cmake("${scpm_work_dir}/wolfssl-${scpm_wolfssl_version}" "-DBUILD_SHARED_LIBS=OFF")
+    file(WRITE ${scpm_work_dir}/wolfssl-${scpm_wolfssl_version}.installed)
+endif()
+
+if(scpm_platform_windows)
+    set(scpm_wolfssl_lib
+        wolfssl
+        CACHE STRING ""
+    )
+    set(scpm_wolfssl_lib_debug
+        wolfssld
+        CACHE STRING ""
+    )
+    set(scpm_wolfssl_depends
+        ""
+        CACHE STRING ""
+    )
+else()
+
+    set(scpm_wolfssl_lib
+        wolfssl
+        CACHE STRING ""
+    )
+    set(scpm_wolfssl_depends
+        ""
+        CACHE STRING ""
+    )
+
+endif()

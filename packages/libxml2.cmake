f066c403c7dd6d1203a63dce8dc6a95631efc791
Pavel Perekhozhikh <p.perehogih@gmail.com>
2025-03-19 18:36:23 +0300

    Fix libxml2 build for win32

--- packages/libxml2.cmake
+++ packages/libxml2.cmake
@@ -3,9 +3,13 @@ if (NOT scpm_llibxml2_version)
 endif()
 set(scpm_libxml2_repo "https://github.com/winlibs/libxml2")
 
+if(scpm_platform_windows)
+    scpm_install(zlib)
+endif()
+
 if (NOT EXISTS ${scpm_work_dir}/libxml2-${scpm_libxml2_version}.installed)
     scpm_download_github_archive("${scpm_libxml2_repo}" "libxml2-${scpm_libxml2_version}")
-    scpm_build_cmake("${scpm_work_dir}/libxml2-libxml2-${scpm_libxml2_version}" "-DLIBXML2_WITH_PROGRAMS=OFF" "-DLIBXML2_WITH_PYTHON=OFF" "-DLIBXML2_WITH_TESTS=OFF")
+    scpm_build_cmake("${scpm_work_dir}/libxml2-libxml2-${scpm_libxml2_version}" "-DLIBXML2_WITH_PROGRAMS=OFF" "-DLIBXML2_WITH_PYTHON=OFF" "-DLIBXML2_WITH_TESTS=OFF" "-DLIBXML2_WITH_ICONV=OFF" "-DLIBXML2_WITH_ICU=OFF" "-DLIBXML2_WITH_LZMA=OFF" "-DLIBXML2_WITH_THREADS=OFF")
     file(WRITE ${scpm_work_dir}/libxml2-${scpm_libxml2_version}.installed "")
 endif()
 

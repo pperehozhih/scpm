8421651ba773ff00ffea99d9bdc1ab6de3f76b46
Pavel Perekhozhikh <p.perehogih@gmail.com>
2025-04-16 14:43:33 +0300

    Fix libusb build type for macos

--- packages/libusb.cmake
+++ packages/libusb.cmake
@@ -5,7 +5,11 @@ set(scpm_libusb_repo "https://github.com/libusb/libusb-cmake")
 
 if (NOT EXISTS ${scpm_work_dir}/libusb-${scpm_libusb_version}.installed)
     scpm_download_github_archive("${scpm_libusb_repo}" "v${scpm_libusb_version}")
-    scpm_build_cmake("${scpm_work_dir}/libusb-cmake-${scpm_libusb_version}")
+    if (scpm_platform_macos)
+        scpm_build_cmake("${scpm_work_dir}/libusb-cmake-${scpm_libusb_version}" "-DLIBUSB_BUILD_SHARED_LIBS=ON")
+    else()
+        scpm_build_cmake("${scpm_work_dir}/libusb-cmake-${scpm_libusb_version}")
+    endif()
     file(WRITE ${scpm_work_dir}/libusb-${scpm_libusb_version}.installed "")
 endif()
 

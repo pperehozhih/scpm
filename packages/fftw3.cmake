a0a7f795140d4bcafbd8cc186b23fbe42c342ee3
Pavel Perekhozhikh <p.perehogih@gmail.com>
2025-04-18 09:10:33 +0300

    Change fftw2 source storage and add float precision

--- packages/fftw3.cmake
+++ packages/fftw3.cmake
@@ -3,16 +3,12 @@
 if (NOT scpm_fftw3_version)
     set(scpm_fftw3_version "3.3.10" CACHE STRING "")
 endif()
-
-set(scpm_fftw3_repo "https://github.com/FFTW/fftw3")
+set(scpm_fftw3_repo "https://fftw.org/")
 
 if (NOT EXISTS ${scpm_work_dir}/fftw3-${scpm_fftw3_version}.installed)
-    scpm_download_github_archive("${scpm_fftw3_repo}" "fftw-${scpm_fftw3_version}")
-    scpm_file_patch("${scpm_work_dir}/fftw3-fftw-${scpm_fftw3_version}/rdft/conf.c" "X(solvtab_exec)(X(solvtab_rdft_r2cf), p);" "")
-    scpm_file_patch("${scpm_work_dir}/fftw3-fftw-${scpm_fftw3_version}/rdft/conf.c" "X(solvtab_exec)(X(solvtab_rdft_r2cb), p);" "")
-    scpm_file_patch("${scpm_work_dir}/fftw3-fftw-${scpm_fftw3_version}/rdft/conf.c" "X(solvtab_exec)(X(solvtab_rdft_r2r), p);" "")
-    scpm_file_patch("${scpm_work_dir}/fftw3-fftw-${scpm_fftw3_version}/dft/conf.c" "X(solvtab_exec)(X(solvtab_dft_standard), p);" "")
-    scpm_build_cmake("${scpm_work_dir}/fftw3-fftw-${scpm_fftw3_version}" "-DENABLE_TESTING=OFF")
+    # scpm_download_github_archive("${scpm_fftw3_repo}" "fftw-${scpm_fftw3_version}")
+    scpm_download_and_extract_archive("${scpm_fftw3_repo}" "fftw-${scpm_fftw3_version}.tar.gz")
+    scpm_build_cmake("${scpm_work_dir}/fftw-${scpm_fftw3_version}" "-DENABLE_TESTING=OFF" "-DENABLE_FLOAT=ON")
     file(WRITE ${scpm_work_dir}/fftw3-fftw-${scpm_fftw3_version}.installed)
 endif()
 

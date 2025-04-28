e4f0dcfcce679c2f4d3fc338e67b7dd1c1d726f2
Pavel Perekhozhikh <p.perehogih@gmail.com>
2025-04-28 18:19:36 +0300

    Change for magic link options

--- packages/magic.cmake
+++ packages/magic.cmake
@@ -1,14 +1,14 @@
 if (NOT scpm_magic_version)
-    set(scpm_magic_version "5.45.0" CACHE STRING "")
+    set(scpm_magic_version "master" CACHE STRING "")
 endif()
 set(scpm_magic_repo "https://gitflic.ru/project/paul2la/file.git")
 
 if (NOT EXISTS ${scpm_work_dir}/magic-${scpm_magic_version}.installed)
     if(EXISTS "${scpm_work_dir}/magic-${scpm_magic_version}")
-        magic(REMOVE_RECURSE "${scpm_work_dir}/magic-${scpm_magic_version}")
+        FILE(REMOVE_RECURSE "${scpm_work_dir}/magic-${scpm_magic_version}")
     endif()
     execute_process(
-        COMMAND git clone -b "CMAKE_${scpm_magic_version}" --depth 1 ${scpm_magic_repo} magic-${scpm_magic_version}
+        COMMAND git clone -b "${scpm_magic_version}" --depth 1 ${scpm_magic_repo} magic-${scpm_magic_version}
         WORKING_DIRECTORY ${scpm_work_dir}
         RESULT_VARIABLE scpm_magic_clone_reult
     )
@@ -16,9 +16,9 @@ if (NOT EXISTS ${scpm_work_dir}/magic-${scpm_magic_version}.installed)
         message(FATAL_ERROR "[SCPM] cannot clone repos ${scpm_magic_repo}")
     endif()
     # Hack for disable build utils
-    set(buildargs "-DBUILD_FILE_UTILS=OFF" "-DBUILD_MEMTEST_UTILS=OFF")
+    set(buildargs "-DBUILD_FILE_UTILS=OFF" "-DBUILD_MEMTEST_UTILS=OFF" "-DBUILD_SHARED_LIBS=OFF")
     scpm_build_cmake("${scpm_work_dir}/magic-${scpm_magic_version}/cmake" "${buildargs}")
-    magic(WRITE ${scpm_work_dir}/magic-${scpm_magic_version}.installed)
+    FILE(WRITE ${scpm_work_dir}/magic-${scpm_magic_version}.installed)
 endif()
 
 set(scpm_magic_lib

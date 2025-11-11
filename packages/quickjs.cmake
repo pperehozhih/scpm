2b103926e2f5062f880da84b82d8e5de9278b716
Pavel Perekhozhikh <p.perehogih@gmail.com>
2025-11-11 23:57:36 +0300

    Add quickjs

---
+++ packages/quickjs.cmake
@@ -0,0 +1,25 @@
+if (NOT scpm_quickjs_version)
+    set(scpm_quickjs_version "2025-09-13" CACHE STRING "")
+endif()
+set(scpm_quickjs_repo "https://github.com/pperehozhih/quickjs")
+
+if (NOT EXISTS ${scpm_work_dir}/quickjs-${scpm_quickjs_version}.installed)
+    scpm_download_github_archive("${scpm_quickjs_repo}" "v${scpm_quickjs_version}")
+    scpm_build_cmake("${scpm_work_dir}/quickjs-${scpm_quickjs_version}")
+    file(WRITE ${scpm_work_dir}/quickjs-${scpm_quickjs_version}.installed)
+endif()
+if(scpm_platform_windows)
+    set(scpm_quickjs_lib
+        CACHE STRING ""
+    )
+    set(scpm_quickjs_lib_debug
+        CACHE STRING ""
+    )
+else()
+    set(scpm_quickjs_lib
+        CACHE STRING ""
+    )
+endif()
+set(scpm_quickjs_depends
+    CACHE STRING ""
+)
